5692fbb93091210dfcffa639de482c9f89a0b1be
Kamo-Git-Hub <53765597+Kamo-Git-Hub@users.noreply.github.com>
2020-03-15 23:42:39 -0700

    Updates

---
+++ precache-manifest.7fd2dd5f70c24226d397937669e5c9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31480ab95c32db11057229c3b61720c",
+    "url": "/todolist-react-redux/index.html"
+  },
+  {
+    "revision": "d12004204e223a29a27b",
+    "url": "/todolist-react-redux/static/css/main.1284d245.chunk.css"
+  },
+  {
+    "revision": "2c368866c7e4a94a77c4",
+    "url": "/todolist-react-redux/static/js/2.25fba06e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/todolist-react-redux/static/js/2.25fba06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12004204e223a29a27b",
+    "url": "/todolist-react-redux/static/js/main.a7dc8ef1.chunk.js"
+  },
+  {
+    "revision": "82ccf28b2f38f7fbfde4",
+    "url": "/todolist-react-redux/static/js/runtime-main.b38e9436.js"
+  }
+]);
